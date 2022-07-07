d65c836f25508bb7bb7693ac657285fddebf2669
Burak Kalkan <burak.kalkan26@gmail.com>
2022-07-07 18:11:03 +0300

    Add project files.

---
+++ planck/Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace planck
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea27 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend27 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series66 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series67 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series68 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series69 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea28 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend28 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series70 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button2 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea27.AxisY.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Number;
+            chartArea27.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea27);
+            legend27.Name = "Legend1";
+            this.chart1.Legends.Add(legend27);
+            this.chart1.Location = new System.Drawing.Point(16, 15);
+            this.chart1.Margin = new System.Windows.Forms.Padding(4);
+            this.chart1.Name = "chart1";
+            series66.ChartArea = "ChartArea1";
+            series66.Legend = "Legend1";
+            series66.Name = "T=5100";
+            series67.ChartArea = "ChartArea1";
+            series67.Legend = "Legend1";
+            series67.Name = "T=7120";
+            series68.ChartArea = "ChartArea1";
+            series68.Legend = "Legend1";
+            series68.Name = "Series3";
+            series69.ChartArea = "ChartArea1";
+            series69.Legend = "Legend1";
+            series69.Name = "T=5780";
+            this.chart1.Series.Add(series66);
+            this.chart1.Series.Add(series67);
+            this.chart1.Series.Add(series68);
+            this.chart1.Series.Add(series69);
+            this.chart1.Size = new System.Drawing.Size(989, 369);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            this.chart1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.chart1_MouseDown);
+            this.chart1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.chart1_MouseMove);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1366, 483);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 28);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Çiz(Katmanlar)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chart2
+            // 
+            chartArea28.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea28);
+            legend28.Name = "Legend1";
+            this.chart2.Legends.Add(legend28);
+            this.chart2.Location = new System.Drawing.Point(1013, 15);
+            this.chart2.Margin = new System.Windows.Forms.Padding(4);
+            this.chart2.Name = "chart2";
+            series70.ChartArea = "ChartArea1";
+            series70.Legend = "Legend1";
+            series70.Name = "T=5780";
+            this.chart2.Series.Add(series70);
+            this.chart2.Size = new System.Drawing.Size(620, 369);
+            this.chart2.TabIndex = 3;
+            this.chart2.Text = "chart2";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1366, 449);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(124, 28);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Çiz(Güneş)";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Enabled = false;
+            this.trackBar1.Location = new System.Drawing.Point(16, 441);
+            this.trackBar1.Margin = new System.Windows.Forms.Padding(4);
+            this.trackBar1.Maximum = 10000;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(1248, 56);
+            this.trackBar1.TabIndex = 5;
+            this.trackBar1.Tag = "";
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(148, 398);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(148, 421);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(389, 409);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "label3";
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Enabled = false;
+            this.trackBar2.Location = new System.Drawing.Point(16, 505);
+            this.trackBar2.Margin = new System.Windows.Forms.Padding(4);
+            this.trackBar2.Maximum = 10000;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(1248, 56);
+            this.trackBar2.TabIndex = 9;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            this.trackBar2.ValueChanged += new System.EventHandler(this.trackBar2_ValueChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(1271, 454);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(88, 21);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.Text = "Ortalama";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(1271, 518);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(96, 21);
+            this.checkBox2.TabIndex = 11;
+            this.checkBox2.Text = "Sıcaklık(K)";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(907, 391);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(86, 21);
+            this.checkBox3.TabIndex = 12;
+            this.checkBox3.Text = "\'e sabitle";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(801, 390);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 13;
+            this.textBox1.Text = "3e+15";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1366, 519);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(124, 28);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Temizle";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1642, 583);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Planck\'s Function";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ planck/Form1.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace planck
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.chart1.MouseWheel += chart1_MouseWheel;
+            textBox1.Enabled = false;
+        }
+        public double wave, BV, h, c, k, XKT, XKT2, XKT3, eu, hclambda, lambdaMax5, lambdaMax7, BV2, BVort, eu2, eu3,Temp;
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            chart1.Series[0].Points.Clear();
+            chart1.Series[1].Points.Clear();
+            chart1.Series[2].Points.Clear();
+            chart1.Series[3].Points.Clear();
+            chart2.Series[0].Points.Clear();
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Enabled == false && checkBox3.Checked == true)
+            {
+                textBox1.Enabled = true;
+                chart1.ChartAreas[0].AxisY.Maximum = Convert.ToDouble(textBox1.Text);
+            }
+            else if(textBox1.Enabled == true && checkBox3.Checked == false)
+            {
+                textBox1.Enabled = false;
+                chart1.ChartAreas[0].AxisY.Maximum = Double.NaN;
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked == true)
+            { trackBar2.Enabled = true; label1.Text = "";label2.Text = "";label3.Text = "";checkBox1.Enabled = false; }
+            else { trackBar2.Enabled = false; checkBox1.Enabled = true; }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkBox1.Checked==true)
+            { trackBar1.Enabled = true; label1.Text = ""; label2.Text = ""; label3.Text = "";checkBox2.Enabled = false; }
+            else { trackBar1.Enabled = false; checkBox2.Enabled = true; }
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+        }
+
+        private void trackBar2_ValueChanged(object sender, EventArgs e)
+        {
+            chart1.Series[2].Points.Clear();
+            Temp = Convert.ToDouble(trackBar2.Value);
+            double X;
+            h = 6.625e-27;
+            c = 2.997e10;
+            k = 1.38e-16;
+            for (X = 0; X <= 2e-4; X = X + 1e-7)
+            {
+                wave = X;
+                XKT3 = (h * c) / (wave * k * Temp);
+                hclambda = Math.Pow(wave, 5);
+                eu3 = Math.Pow(Math.E, XKT3); 
+                BVort = (2 * ((h * c * c) / hclambda)) / (eu3 - 1);
+                chart1.Series[2].Points.AddXY(X, BVort);
+            }
+            chart1.Series[2].Name = "T=" + Temp;
+            chart1.Series[2].ChartType = SeriesChartType.FastLine;
+            chart1.Series[2].Color = Color.Crimson;
+        }
+
+        public static double CalculateSlope(Point a, Point b)
+        {
+            if (b.Y == a.Y)
+                return double.PositiveInfinity;
+
+            if (b.X == a.X)
+                return 0.0;
+
+            return (Convert.ToDouble(b.Y) - Convert.ToDouble(a.Y)) / (Convert.ToDouble(b.X) - Convert.ToDouble(a.X));
+        }
+
+        private void chart1_MouseDown(object sender, MouseEventArgs e)
+        {
+            chart1.Series[0].ToolTip = "#VALY, #VALX";
+        }
+
+        private void chart1_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+
+        }
+        double a, b, Sıcort;
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            chart1.Series[0].Points.Clear();
+            chart1.Series[1].Points.Clear();
+            chart1.Series[2].Points.Clear();
+            a = Convert.ToDouble(trackBar1.Value) * 1e-4;
+            b = 1 - a;
+            label1.Text = (Math.Round(a, 4)).ToString() + " * βλ(T=5100)";
+            label2.Text = (Math.Round(b, 4)).ToString() + " * βλ(T=7120)";
+            double X;
+            h = 6.625e-27;
+            c = 2.997e10;
+            k = 1.38e-16;
+            for (X = 0; X <= 2e-4; X = X + 1e-7)
+            {
+                wave = X;
+                XKT = (h * c) / (wave * k * 5100);
+                XKT2 = (h * c) / (wave * k * 7120);
+                hclambda = Math.Pow(wave, 5);
+                eu = Math.Pow(Math.E, XKT);
+                BV = (2 * ((h * c * c) / hclambda)) / (eu - 1);
+                eu2 = Math.Pow(Math.E, XKT2);
+                BV2 = (2 * ((h * c * c) / hclambda)) / (eu2 - 1);
+                BVort = a * BV + b * BV2;
+                chart1.Series[0].Points.AddXY(X, BV);
+                chart1.Series[1].Points.AddXY(X, BV2);
+                chart1.Series[2].Points.AddXY(X, BVort);
+            }
+
+            Sıcort = 0.28977721 / (chart1.Series[2].Points.FindMaxByValue().XValue);
+
+            chart1.Series[2].Name = "T=" + Math.Round(Sıcort, 1);
+            chart1.Series[0].ChartType = SeriesChartType.FastLine;
+            chart1.Series[1].ChartType = SeriesChartType.FastLine;
+            chart1.Series[2].ChartType = SeriesChartType.FastLine;
+            chart1.Series[0].Color = Color.Black;
+            chart1.Series[1].Color = Color.Black;
+            chart1.Series[2].Color = Color.Purple;
+            chart1.Series[1].MarkerSize = 1;
+            chart1.Series[0].MarkerSize = 1;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            double X;
+            h = 6.625e-27;
+            c = 2.997e10;
+            k = 1.38e-16;
+            if (chart1.Series[3].Points.Count == 0)
+            {
+                for (X = 0; X <= 2e-4; X = X + 1e-6)
+                {
+                    wave = X;
+                    XKT = (h * c) / (wave * k * 5780);
+                    hclambda = Math.Pow(wave, 5);
+                    eu = Math.Pow(Math.E, XKT);
+                    BV = (2 * ((h * c * c) / hclambda)) / (eu - 1);
+                    chart2.Series[0].Points.AddXY(X, BV);
+                    chart1.Series[3].Points.AddXY(X, BV);
+                }
+                chart2.Series[0].ChartType = SeriesChartType.FastLine;
+                chart1.Series[3].ChartType = SeriesChartType.FastLine;
+                chart2.Series[0].Color = Color.Black;
+                chart1.Series[3].Color = Color.Red;
+                chart2.Series[0].MarkerSize = 3;
+            }
+            else {
+                chart2.Series[0].Points.Clear();
+                chart1.Series[3].Points.Clear();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            double X;
+            h = 6.625e-27;
+            c = 2.997e10;
+            k = 1.38e-16;
+
+
+            if (chart1.Series[0].Points.Count == 0 && chart1.Series[1].Points.Count == 0)
+            {
+                for (X = 0; X <= 2e-4; X = X + 1e-6)
+                {
+                    wave = X;
+                    XKT = (h * c) / (wave * k * 5100);
+                    XKT2 = (h * c) / (wave * k * 7120);
+                    hclambda = Math.Pow(wave, 5);
+                    eu = Math.Pow(Math.E, XKT);
+                    BV = (2 * ((h * c * c) / hclambda)) / (eu - 1);
+                    eu2 = Math.Pow(Math.E, XKT2);
+                    BV2 = (2 * ((h * c * c) / hclambda)) / (eu2 - 1);
+                    lambdaMax5 = 0.6666666666666;
+                    lambdaMax7 = 0.3333333333333;
+                    BVort = lambdaMax5 * BV + lambdaMax7 * BV2;
+                    chart1.Series[0].Points.AddXY(X, BV);
+                    chart1.Series[1].Points.AddXY(X, BV2);
+                }
+                chart1.Series[0].ChartType = SeriesChartType.FastLine;
+                chart1.Series[1].ChartType = SeriesChartType.FastLine;
+                chart1.Series[0].Color = Color.Black;
+                chart1.Series[1].Color = Color.Black;
+                chart1.Series[1].MarkerSize = 1;
+                chart1.Series[0].MarkerSize = 1;
+            }
+            else if(chart1.Series[0].Points.Count != 0 && chart1.Series[0].Points.Count != 0) {
+                chart1.Series[0].Points.Clear();
+                chart1.Series[1].Points.Clear();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            chart1.Series[0].Color = Color.Black;
+            chart1.Series[1].Color = Color.Black;
+            chart1.Series[2].Color = Color.Purple;
+            chart1.Series[1].MarkerSize = 3;
+            chart1.Series[0].MarkerSize = 3;
+            chart2.Series[0].ChartType = SeriesChartType.FastLine;
+            chart1.Series[3].ChartType = SeriesChartType.FastLine;
+            chart2.Series[0].Color = Color.Black;
+            chart1.Series[3].Color = Color.Red;
+            chart2.Series[0].MarkerSize = 3;
+            label1.Text = "";
+            label2.Text = "";
+            label3.Text = "";
+            this.AutoSize = false;
+        }
+        private void chart1_MouseWheel(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                if (e.Delta < 0)
+                {
+                    chart1.ChartAreas[0].AxisX.ScaleView.ZoomReset();
+                    chart1.ChartAreas[0].AxisY.ScaleView.ZoomReset();
+                }
+
+                if (e.Delta > 0)
+                {
+                    double xMin = chart1.ChartAreas[0].AxisX.ScaleView.ViewMinimum;
+                    double xMax = chart1.ChartAreas[0].AxisX.ScaleView.ViewMaximum;
+                    double yMin = chart1.ChartAreas[0].AxisY.ScaleView.ViewMinimum;
+                    double yMax = chart1.ChartAreas[0].AxisY.ScaleView.ViewMaximum;
+
+                    double posXStart = chart1.ChartAreas[0].AxisX.PixelPositionToValue(e.Location.X) - (xMax - xMin) / 4;
+                    double posXFinish = chart1.ChartAreas[0].AxisX.PixelPositionToValue(e.Location.X) + (xMax - xMin) / 4;
+                    double posYStart = chart1.ChartAreas[0].AxisY.PixelPositionToValue(e.Location.Y) - (yMax - yMin) / 4;
+                    double posYFinish = chart1.ChartAreas[0].AxisY.PixelPositionToValue(e.Location.Y) + (yMax - yMin) / 4;
+
+
+                    chart1.ChartAreas[0].AxisX.ScaleView.Zoom(posXStart, posXFinish);
+                    chart1.ChartAreas[0].AxisY.ScaleView.Zoom(posYStart, posYFinish);
+                }
+            }
+            catch { }
+        }
+    }
+}
